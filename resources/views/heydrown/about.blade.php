a816e68a6df35fccdda53a03f1684ff61cccfe67
rizkyzet <rizkyzetzet121@gmail.com>
2021-11-17 16:21:01 +0700

    fixing padding about

--- resources/views/heydrown/about.blade.php
+++ resources/views/heydrown/about.blade.php
@@ -5,13 +5,13 @@
         <h1 class="font-weight-bold text-white heading" data-aos="fade-in" data-aos-duratuin="3000">HEYDROWN</h1>
     </div>
 
-    <div class="container-fluid container-about p-0">
+    <div class="container-fluid container-about p-4">
         <div class="row row-cols-1 row-cols-sm-1 row-cols-md-2 row-cols-lg-2">
-            <div class="col p-0">
+            <div class="col p-4">
                 <img src="/img/about-2.jpg" alt="" data-aos="fade-right" class="img-fluid">
             </div>
 
-            <div class="col p-5 d-flex justify-content-center flex-column about-description ">
+            <div class="col p-4 d-flex justify-content-center flex-column about-description ">
                 <h3 class="font-weight-bold text-white heading-text-column">OUR JOURNEY</h3>
                 <p class="text-white">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Alias ipsa sint cum,
                     nobis molestiae beatae?
@@ -20,12 +20,12 @@
             </div>
         </div>
         <div class="row row-cols-1 row-cols-sm-1 row-cols-md-2 row-cols-lg-2">
-            <div class="col order-1 order-sm-1 order-md-2 order-lg-2 p-0">
+            <div class="col order-1 order-sm-1 order-md-2 order-lg-2 p-4">
                 <img src="/img/about-3.jpg" alt="" data-aos="fade-left" class="img-fluid">
             </div>
 
             <div
-                class="col order-2 order-sm-2 order-md-1 order-lg-1 p-5 d-flex justify-content-center flex-column about-description">
+                class="col order-2 order-sm-2 order-md-1 order-lg-1 p-4 d-flex justify-content-center flex-column about-description">
                 <h3 class="font-weight-bold text-white heading-text-column">VISION</h3>
                 <p class="text-white">Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis nobis voluptate
                     repudiandae omnis culpa voluptas odit ipsam libero voluptates minima excepturi architecto, ab aliquam
@@ -34,11 +34,11 @@
             </div>
         </div>
         <div class="row row-cols-1 row-cols-sm-1 row-cols-md-2 row-cols-lg-2">
-            <div class="col p-0 d-flex align-items-center">
+            <div class="col p-4 d-flex align-items-center">
                 <img src="/img/about-4.jpg" alt="" data-aos="fade-right" class="img-fluid">
             </div>
 
-            <div class="col p-5 d-flex justify-content-center flex-column p-0 about-description">
+            <div class="col d-flex justify-content-center flex-column p-4 about-description">
                 <h3 class="font-weight-bold text-white heading-text-column">GET IN TOUCH</h3>
                 <p class="text-white">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Magnam, exercitationem
                     beatae aspernatur saepe, rem sequi libero nemo distinctio aliquid architecto itaque autem perspiciatis
